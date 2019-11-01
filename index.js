b7f6bdbd0136b3d97c2786d68e5ae57670deecce
umekow <umekolashun@gmail.com>
2019-11-01 10:31:50 -0500

    created comments

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
-
-Sing along:
-
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
-
-Go code!
-*/
-
-
+/******SETUP*************/
 require('dotenv').config(); 
 
 const express = require('express');
+const helmet = require('helmet'); 
+const morgan = require('morgan'); 
 const server = express(); 
 server.use(express.json()); 
-
-
+server.use(helmet);
+server.use(morgan); 
 const port = process.env.PORT || 5000; 
+
+/*********END OF SETUP********/
+
+/*********MIDDLEWARE************/
+
+
+/*********END OF MIDDLEWARE************/
+
+/*********POST************/
+
+
+/*********GET************/
+
+/*********PUT************/
+
+/*********DELETE************/
 
 server.listen(port, () => console.log(`Wohoo! Server is running on http://localhost:${port}`));
