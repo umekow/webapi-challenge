e69e481c0a04a1303a161a26f4de68eef2ea120b
umekow <umekolashun@gmail.com>
2019-11-01 10:49:55 -0500

    added middleware

--- index.js
+++ index.js
@@ -5,22 +5,53 @@ const express = require('express');
 const helmet = require('helmet'); 
 const morgan = require('morgan'); 
 const server = express(); 
+const projects = require('./data/helpers/projectModel'); 
+const actions = require('./data/helpers/actionModel'); 
 server.use(express.json()); 
-server.use(helmet);
-server.use(morgan); 
+server.use(helmet());
+server.use(morgan()); 
 const port = process.env.PORT || 5000; 
 
 /*********END OF SETUP********/
 
+
+
+
 /*********MIDDLEWARE************/
 
 
-/*********END OF MIDDLEWARE************/
+function validateProjectID(req, res, next) {
+    projects.get(req.params.id)
+    .then(r => {
+        if(r) {
+            req.body = r; 
+            next(); 
+        }else{
+            res.status(400).json({message : "A project with that id does not exist"}); 
+        }
+    }).catch(
+        error => res.status(400).json(error)
+    )
+
+
+}
+
+/*******END OF MIDDLEWARE******/
+
+
+
+
+/*******************************************REQUESTS*****************************/
 
 /*********POST************/
 
 
 /*********GET************/
+server.get('/', (req, res) => {
+    projects.get()
+    .then(r => res.status(200).json(r))
+    .catch(error => res.status(400).json(error))
+})
 
 /*********PUT************/
 
